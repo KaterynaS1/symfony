58a4e1fc33e6c6944a9480ccbcfef4990afea00b
KaterynaS1 <katerynastepchuk@gmail.com>
2025-11-30 13:13:24 +0100

    fixed DB + fixures

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use App\Entity\Article;
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $article = new Article();
+        $article -> setDescription(description: 'Wuthering Heist" is an episode of the British anthology series Inside No. 9, not a story from Wuthering Heights. The plot involves a criminal named Pantalone hiring a crew to steal diamonds, but he has assembled a team of double-crossing clowns, leading to a chaotic and disastrous heist.');
+        $article -> setTitle(title: 'wuthering heist');
+        $manager->persist($article);
+
+    }
+}
